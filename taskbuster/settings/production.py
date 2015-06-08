c63c3d3308c84ba55ec757dd06f2fcbd3eda0256
Alex Terela <alex.terela@ARTIVEST-MNI-02.local>
2015-06-08 17:17:07 -0400

    Taskbuster project created

---
+++ taskbuster/settings/production.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from .base import *
+
+# set debug to false for production
+DEBUG = False
---
+++ taskbuster/settings/staging.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from .base import *
+
+# set debug to false for staging
+DEBUG = False
